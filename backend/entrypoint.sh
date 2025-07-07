e0c8964d2289b42cc5cccd8ecf44859458766127
Edgar Jimenez <ebyted@gmail.com>
2025-07-07 00:37:26 -0700

    Fix: Add PGPASSWORD variable to psql commands in entrypoint script for database access

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -10,12 +10,12 @@ done
 echo "PostgreSQL está listo."
 
 # Comprobar si la tabla businesses existe
-EXISTS=$(psql -h db -U postgres -d maestro_inventario -tAc "SELECT to_regclass('public.businesses') IS NOT NULL;")
+EXISTS=$(PGPASSWORD=postgres12 psql -h db -U postgres -d maestro_inventario -tAc "SELECT to_regclass('public.businesses') IS NOT NULL;")
 
 if [ "$EXISTS" = "f" ]; then
   echo "La base de datos está vacía. Restaurando backup.sql..."
   if [ -f /app/backup.sql ]; then
-    psql -h db -U postgres -d maestro_inventario -f /app/backup.sql
+    PGPASSWORD=postgres psql -h db -U postgres -d maestro_inventario -f /app/backup.sql
     alembic stamp head
   else
     echo "No se encontró /app/backup.sql."
