22a589fb1f3c531256ca971b578b009facca982f
Edgar Jimenez <ebyted@gmail.com>
2025-07-09 12:42:56 -0700

    DOCKER Y DOCKER-COMPOSE

--- frontend/SimpleApp.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SimpleNavigator from './src/navigation/SimpleNavigator';
-import './src/i18n'; // Initialize i18n
-
-export default function App() {
-  console.log('🚀 App iniciada - Modo Simple');
-  
-  return <SimpleNavigator />;
-}
--- frontend/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Configuración específica para web
-config.resolver.platforms = ['ios', 'android', 'web'];
-
-module.exports = config;
