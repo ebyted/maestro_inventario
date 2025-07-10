f1b9acdc12a4cdfc1bd709a9e3c5109440c87e9c
Edgar Jimenez <ebyted@gmail.com>
2025-07-10 03:08:10 -0700

    Refactor code structure for improved readability and maintainability

--- backend/prueba.py
+++ backend/prueba.py
@@ -5,5 +5,15 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # Genera el hash
 password_hash = pwd_context.hash("admin123")
-
 print("Hash generado:", password_hash)
+
+# Verifica el hash (cambia este valor por el hash guardado en tu base de datos)
+hash_guardado = password_hash  # O pega aquí el hash de la base de datos
+hash_guardado = '$2b$12$mEGX8Kq6XfXrstNo1A0Ga.rm3ofvCi3HEHTEoFsWjQEZ2QhLnjaa.'  # O pega aquí el hash de la base de datos
+
+
+# Validar la contraseña
+if pwd_context.verify("admin123", hash_guardado):
+    print("✅ La contraseña es válida para el hash.")
+else:
+    print("❌ La contraseña NO coincide con el hash.")
